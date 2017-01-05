39028bbf0a9b1b43b84875141af3ec5fefc82375
Maxxx88 <maxence.alphonse@gmail.com>
2017-01-06 02:07:59 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,78 @@
-## Put comments here that give an overall description of what your
-## functions do
+## -------------------------------------------
+##
+## Created by: Max
+## Created date: 05/01/2016
+## Description: Create list of functions for later inverse a matrix
+## Return: List of functions
+## Input: A matrix but need a square matrix for later
+##
+## Example of input: matrix(rexp(400, rate=.1), ncol=20)
+##                   matrix(rexp(200, rate=.1), ncol=20) for none square matrix
+##
+## Modification date:
+## Modify by: 
+##
+## -------------------------------------------
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cached <- NULL  # init the cache value to null
+  
+  set <- function(y) {
+    x <<- y
+    cached <<- NULL
+  }
+  
+  get <- function() x
+  setMatrix <- function(inverse) cached <<- inverse
+  getInverse <- function() cached
+  
+  # return teh function to teh env
+  list(set = set, get = get, setMatrix = setMatrix, getInverse = getInverse)
 }
 
+## End makeCacheMatrix
 
-## Write a short comment describing this function
+
+
+## -------------------------------------------
+##
+## Created by: Max
+## Created date: 05/01/2016
+## Description: Calculate teh inverted matrix
+## Return: Matrix
+## Input: A matrix that needs to be a square matrix
+##
+## Modification date:
+## Modify by: 
+##
+## -------------------------------------------
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cached <- x$getInverse()
+  
+  # 1- create a matrix
+  matrix <- x$get()
+  
+  # 2- Check taht the matrix is square
+  # If not, basic error message management
+  tryCatch( {
+    # Return inversed matrix
+    cached <- solve(matrix, ...)
+  },
+  # Error management
+  error = function(error) {
+    message(error, "") 
+  },
+  # Warning management
+  warning = function(warning) {
+    message(warning, "")
+  })
+  
+  # Return the inverted matrix
+  return (cached)
 }
+
+## End cacheSolve
